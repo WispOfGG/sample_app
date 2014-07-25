e7d3e05cdae56086c4cab85e4c022db9a7aca2dd
WispOfGG <lausreport@gmail.com>
2014-07-25 01:01:24 -0600

    addjustment

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 group :development do
 	gem 'rspec-rails'
+	gem 'capybara'
 end
 
 group :test do
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, :type => :controller do
+
+  render_views
+
+  describe "GET home" do
+    it "returns http success" do
+      get :home
+      expect(response).to be_success
+    end
+    it "it should return a title" do
+      get :home
+      expect(response).to have_selector("title", :content =>"This is the title")
+  end
+end
+  describe "GET contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET about" do
+    it "returns http success" do
+      get :about
+      expect(response).to be_success
+    end
+  end
+
+end
